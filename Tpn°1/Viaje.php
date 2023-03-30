d79b1c9891b582a49d262c82d5b01ecbdc65e353
geronimovillaverde <94569351+geronimovillaverde@users.noreply.github.com>
2023-03-30 15:28:50 -0300

    creando la clase Viaje

--- Tpn°1/Viaje.php
+++ Tpn°1/Viaje.php
@@ -1,4 +1,14 @@
 <?php
 
-echo "Hola github\n";
+/*Importante: Deben enviar el link a la resolución en su repositorio en GitHub del ejercicio.
+La empresa de Transporte de Pasajeros “Viaje Feliz” quiere registrar la información referente a sus viajes. De cada viaje se precisa almacenar el código del mismo, destino, cantidad máxima de pasajeros y los pasajeros del viaje.
+Realice la implementación de la clase Viaje e implemente los métodos necesarios para modificar los atributos de dicha clase (incluso los datos de los pasajeros). Utilice un array que almacene la información correspondiente a los pasajeros. Cada pasajero es un array asociativo con las claves “nombre”, “apellido” y “numero de documento”.
+Implementar un script testViaje.php que cree una instancia de la clase Viaje y presente un menú que permita cargar la información del viaje, modificar y ver sus datos. */
+
+class Viaje{
+    private $codigo;
+    private $destino;
+    private $cantMaxPasajeros;
+    private $pasajeros;
+}
 
